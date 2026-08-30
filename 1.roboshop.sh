3d3062df941245f16a3e39d08b917f7f6b5ee2de
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-30 13:13:49 +0530

    creating instances & extracting IPs

--- 1.roboshop.sh
+++ 1.roboshop.sh
@@ -20,6 +20,14 @@ SG_ID="sg-0c13f122aeebc9259"
 for instance in $@
 do
    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --output text --query 'Instances[0].InstanceId')
-done
+
+if [ INSTANCE_ID -ne "frontend" ]; then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --output text --query 'Reservations[0].Instances[0].PrivateIpAddress')
+else
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
+fi
 
 echo "$instance : $INSTANCE_ID"
+
+done
+
