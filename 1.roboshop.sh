ae267394824fc01d2a2428a2b740fdf4d13503b4
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-30 13:03:07 +0530

    creating instances & extracting IPs

--- 1.roboshop.sh
+++ 1.roboshop.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# # version-1
+# # ==========
+# AMI_ID="ami-0220d79f3f480ecf5" 
+# SG_ID="sg-0c13f122aeebc9259"
+
+# for instance in $@
+# do
+#    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --output text --query 'Instances[0].InstanceId')
+# done
+
+# echo "$instance : $INSTANCE_ID"
+
+# version-1
+# ==========
 AMI_ID="ami-0220d79f3f480ecf5" 
 SG_ID="sg-0c13f122aeebc9259"
 
