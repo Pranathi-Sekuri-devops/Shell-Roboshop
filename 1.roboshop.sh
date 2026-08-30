9822ba2c0eed0ac387c872206aa9f9b33e2b6768
Pranathi-Sekuri-devops <sekuripranathi33753@gmail.com>
2026-08-30 15:18:30 +0530

    creating instances & extracting IPs

--- 1.roboshop.sh
+++ 1.roboshop.sh
@@ -21,13 +21,13 @@ for instance in $@
 do
    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --output text --query 'Instances[0].InstanceId')
 
-if [ $instance -ne "frontend" ]; then
+if [ $instance != "frontend" ]; then # != will be used for string comaparison
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --output text --query 'Reservations[0].Instances[0].PrivateIpAddress')
 else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --output text --query 'Reservations[0].Instances[0].PublicIpAddress')
 fi
 
-echo "$instance : $INSTANCE_ID : $IP"
+echo "$instance : $IP"
 
 done
 
